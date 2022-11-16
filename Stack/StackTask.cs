9170d1f90e4dff0cc10ece5de11956dbda761156
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2022-11-16 23:05:23 +0200

    Добавьте файлы проекта.

---
+++ Stack/StackTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Markup;
+
+namespace Stack
+{
+    public class StackTask<T>
+    {
+        private List<T> data = new List<T>();
+        public int Count => data.Count;
+
+        public void Push(T inputdata)
+        {
+            data.Add(inputdata);
+        }
+
+        public T Pop()
+        {
+            data.RemoveAt(Count);
+            return data.LastOrDefault();
+        }
+
+        public void Clear()
+        {
+            data.Clear();
+        }
+
+        public T Peek() 
+        {
+            return data.LastOrDefault();
+        }
+
+        public T[] CopyTo()
+        {
+            T[] myarray = new T[data.Count];
+            return myarray;
+        }
+    }
+}
